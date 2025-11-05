54381fe64ffca7b167c9e81c6d72a8e644cd55c6
NehemiaOroh <orohnehemia@gmail.com>
2025-11-05 11:16:51 +0700

    Nested Inheritance

---
+++ src/technicallynamedoesntmatter/Main.java
@@ -0,0 +1,14 @@
+package technicallynamedoesntmatter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar= new Car("Honda Civic");
+		myCar.showVehicleInfo();
+		myCar.start();
+		myCar.stop();
+		
+	}
+
+}
